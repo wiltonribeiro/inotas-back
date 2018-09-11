d6c9848c833c193c1b4a68490e0243474c3e80e7
Wilton Neto <wiltonribeiro@users.noreply.github.com>
2018-09-10 22:16:48 -0300

    added function that alread return the error model
    That help to not keep repeating the model error initialization just to see whats is happening in each case.

--- models/Error.go
+++ models/Error.go
@@ -1,6 +1,15 @@
 package models
 
+import "fmt"
+
 type Error struct {
 	Message string `json:"message"`
 	Code int `json:"code"`
+}
+
+func ErrorResponse(err error, code int) (Error){
+	return Error{
+		Code:code,
+		Message:fmt.Sprint(err),
+	}
 }
