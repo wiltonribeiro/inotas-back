852ffcdf49319d18dffa6fd47318e2e32c75e8a9
Wilton Neto <wiltonribeiro@users.noreply.github.com>
2018-09-17 17:54:15 -0300

    city id is actually a int

--- models/User.go
+++ models/User.go
@@ -3,7 +3,7 @@ package models
 type User struct {
 	Email string `json:"email"`
 	Name string `json:"name"`
-	CityId string `json:"city_id"`
+	CityId int `json:"city_id"`
 	StateInitials string `json:"state_initials"`
 	Password string `json:"password"`
 } 
