ec9a12df554c7ea4b18e370a80c50e24f0b81ee8
Wilton Neto <wiltonribeiro@users.noreply.github.com>
2018-09-17 17:54:53 -0300

    adapting to connect with online database on google cloud

--- database/connection.go
+++ database/connection.go
@@ -2,21 +2,28 @@ package database
 
 import (
 	_ "github.com/lib/pq"
+	_ "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
 	"fmt"
 	"database/sql"
 )
 
 type Connection struct {
+	dbHost string
+	dbName string
 	dbUser string
 	dbPassword string
-	dbName string
 	con *sql.DB
 }
 
-func CreateConnection(dbUser,dbPassword,dbName string) (Connection,error) {
-	c := Connection{dbUser, dbPassword, dbName, nil}
-	dbInfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", c.dbUser, c.dbPassword, c.dbName)
-	db, err := sql.Open("postgres", dbInfo)
+func CreateConnection(dbHost,dbName,dbUser,dbPassword string) (Connection,error) {
+
+	//dbInfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", c.dbUser, c.dbPassword, c.dbName)
+	//db, err := sql.Open("postgres", dbInfo)
+
+	c := Connection{dbHost, dbName, dbUser, dbPassword, nil}
+	dsn := fmt.Sprintf("host=%s dbname=%s user=%s password=%s sslmode=disable",c.dbHost, c.dbName, c.dbUser, c.dbPassword)
+	db, err := sql.Open("cloudsqlpostgres", dsn)
+
 	c.con = db
 	return  c,err
 }
