b045c2fec531f058817f6957d601f37d1675b02d
Wilton Neto <wiltonribeiro@users.noreply.github.com>
2018-10-21 20:33:57 -0300

    routes doesn't need more the database reference from the server

--- models/Route.go
+++ models/Route.go
@@ -2,9 +2,8 @@ package models
 
 import (
 	"github.com/kataras/iris"
-	"inotas-back/database"
 )
 
 type Route struct {
-	ApplyRoute func(application* iris.Application, con* database.Connection)
+	ApplyRoute func(application* iris.Application)
 }
