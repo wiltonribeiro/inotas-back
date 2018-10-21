fdc39fba6f87eea421b035a62feb54f0a5c47f00
Wilton Neto <wiltonribeiro@users.noreply.github.com>
2018-10-21 20:31:09 -0300

    refactoring connection to allow open connection in each query by the DAOs.

--- database/connection.go
+++ database/connection.go
@@ -1,10 +1,11 @@
-package database
+package Database
 
 import (
 	_ "github.com/lib/pq"
 	_ "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
 	"fmt"
 	"database/sql"
+	"inotas-back/enviroment"
 )
 
 type Connection struct {
@@ -15,16 +16,10 @@ type Connection struct {
 	con *sql.DB
 }
 
-func CreateConnection(dbHost,dbName,dbUser,dbPassword string) (Connection,error) {
-
-	c := Connection{dbHost, dbName, dbUser, dbPassword, nil}
-	//dbInfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", c.dbUser, c.dbPassword, c.dbName)
-	//db, err := sql.Open("postgres", dbInfo)
-
-
+func OpenConnection() (Connection,error) {
+	c := Connection{enviroment.DbHost, enviroment.DbName, enviroment.DbUser, enviroment.DbPassword, nil}
 	dsn := fmt.Sprintf("host=%s dbname=%s user=%s password=%s sslmode=disable",c.dbHost, c.dbName, c.dbUser, c.dbPassword)
 	db, err := sql.Open("cloudsqlpostgres", dsn)
-
 	c.con = db
 	return  c,err
 }
@@ -33,6 +28,9 @@ func (c Connection) Close(){
 	c.con.Close()
 }
 
-func (c Connection) GetDB() *sql.DB{
+func (c Connection) GetDB() (*sql.DB){
 	return c.con
 }
+
+//dbInfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", c.dbUser, c.dbPassword, c.dbName)
+//db, err := sql.Open("postgres", dbInfo)
