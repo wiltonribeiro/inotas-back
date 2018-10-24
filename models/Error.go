579e99999f82dc39c555e1e4ac5e53edbc71c25e
Wilton Neto <wiltonribeiro@users.noreply.github.com>
2018-10-24 01:57:12 -0300

    Erros now can be writing in a log file to get the respective erros case needed

--- models/Error.go
+++ models/Error.go
@@ -1,18 +1,42 @@
 package models
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+	"encoding/json"
+)
 
 type Error struct {
 	Message string `json:"message"`
 	Code int `json:"code"`
+	Time time.Time `json:"time"`
 }
 
 func ErrorResponse(err error, code int) (Error){
-
-	response := Error{
-		Code:code,
-		Message:fmt.Sprint(err),
-	}
-	fmt.Println(response)
+	response := Error{Code:code, Message:fmt.Sprint(err), Time: time.Now()}
+	response.writeLog()
 	return response
+}
+
+func (error Error) writeLog(){
+	exits := error.existFile()
+	f, err := os.OpenFile("errors.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err == nil {
+		data,_ := json.Marshal(error)
+		if exits {
+			f.Write([]byte(","))
+			f.Write([]byte(data))
+		} else {
+			f.Write([]byte(data))
+		}
+		defer  f.Close()
+	}
+}
+
+func (error Error) existFile() bool{
+	if _, err := os.Stat("errors.json"); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
