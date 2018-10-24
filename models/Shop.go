c19a6c7291861934931a6b82dc7ae5b7be590167
Wilton Neto <wiltonribeiro@users.noreply.github.com>
2018-10-24 01:51:26 -0300

    created the ShopRequest
    The ShopRequest was created to return from some http request the essential data to the user in the front,

--- models/Shop.go
+++ models/Shop.go
@@ -10,15 +10,13 @@ type Shop struct {
 	TotalCost float64 `json:"total_cost"`
 }
 
-type ShopComplete struct {
+type ShopRequest struct {
 	NFeKey string `json:"nfe_key"`
 	Alias string `json:"alias"`
-	UserEmail string `json:"user_email"`
-	SellerCnpj string `json:"seller_cnpj"`
 	Date string `json:"date"`
 	Payment string `json:"payment"`
 	TotalCost float64 `json:"total_cost"`
-	Seller Seller `json:"seller"`
+	Seller SellerMinimum `json:"seller"`
 }
 
 
